1d0d8db6aa6ce125a9cf400559f1fad3dea390a1
Franciszek Krawczyk <franciszek2005@gmail.com>
2026-04-16 21:17:35 +0200

    edit: started work on RoomController

--- RoomReservationApi/RoomReservationApi/Controllers/RoomsController.cs
+++ RoomReservationApi/RoomReservationApi/Controllers/RoomsController.cs
@@ -1,12 +1,51 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using RoomReservationApi.Services;
 
 namespace RoomReservationApi.Controllers;
-
-public class RoomsController : Controller
+[ApiController]
+[Route("/api/rooms")]
+public class RoomsController : ControllerBase
 {
-    // GET
-    public IActionResult Index()
+    
+    private IRoomService _roomService;
+    [HttpGet]
+    public IActionResult Get()
     {
-        return View();
+        return Ok(_roomService.GetAll());
     }
+
+    [HttpGet("{id:long}")]
+    public IActionResult GetById(long id)
+    {
+        try
+        {
+            return Ok(_roomService.GetById(id));
+        }
+        catch (ArgumentException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (InvalidOperationException e)
+        {
+            Console.WriteLine(e.Message);
+            return StatusCode(500);
+        }
+    }
+
+    [HttpGet("building/{buildingCode}")]
+    public IActionResult GetByBuildingCode(string buildingCode)
+    {
+        try
+        {
+            return Ok(_roomService.GetByBuildingCode(buildingCode));
+        }
+        catch (ArgumentException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+    public 
+    
+    
 }
