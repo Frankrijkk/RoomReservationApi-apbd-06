cc3e70a1acd23d956be7400ef4cca001871623da
Franciszek Krawczyk <franciszek2005@gmail.com>
2026-04-16 21:17:04 +0200

    edit: changed the model a little bit

--- RoomReservationApi/RoomReservationApi/Models/Room.cs
+++ RoomReservationApi/RoomReservationApi/Models/Room.cs
@@ -2,21 +2,23 @@ namespace RoomReservationApi.Models;
 
 public class Room
 {
-    public Room(string name, string buildingCode, int floor, bool hasProjector, bool isActive)
+    public Room(string name, string buildingCode, int floor, int capacity, bool hasProjector, bool isActive)
     {
         Id = Interlocked.Increment(ref _currentId);
         Name = name;
         BuildingCode = buildingCode;
         Floor = floor;
+        Capacity = capacity;
         HasProjector = hasProjector;
         IsActive = isActive;
     }
 
     private static long _currentId = 1;
     public long Id { get; private set; }
-    public string Name { get; private set; }
-    public string BuildingCode { get; private set; }
-    public int Floor { get; private set; }
+    public string Name { get; set; }
+    public string BuildingCode { get; set; }
+    public int Floor { get; set; }
+    public int Capacity { get; set; }
     public bool HasProjector { get;  set; }
-    public bool IsActive { get; private set; }
+    public bool IsActive { get; set; }
 }
