5513dfe1489f231ae023df2fed5a72e3d3c24d30
Franciszek Krawczyk <franciszek2005@gmail.com>
2026-04-17 14:11:43 +0200

    changed Reservation set access modifiers

--- RoomReservationApi/RoomReservationApi/Models/Reservation.cs
+++ RoomReservationApi/RoomReservationApi/Models/Reservation.cs
@@ -16,12 +16,12 @@ public class Reservation
 
     private static long _currentId = 0;
     public long Id { get; private set; }
-    public long RoomId { get; private set; }
-    public string OrganizerName {get; private set;}
-    public string Topic { get; private set; }
-    public DateOnly Date { get; private set; }
-    public TimeOnly StartTime { get; private set; }
-    public TimeOnly EndTime { get; private set; }
-    public ReservationStatus Status { get; private set; }
+    public long RoomId { get; set; }
+    public string OrganizerName {get; set;}
+    public string Topic { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public ReservationStatus Status { get; set; }
     
 }
