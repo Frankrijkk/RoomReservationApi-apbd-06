eb5d908ab33dab3dcd0d4a8d049bd1f0e02c4699
Franciszek Krawczyk <franciszek2005@gmail.com>
2026-04-17 12:57:13 +0200

    add: skeleton for Reservation Service

--- RoomReservationApi/RoomReservationApi/Services/IReservationService.cs
+++ RoomReservationApi/RoomReservationApi/Services/IReservationService.cs
@@ -1,6 +1,12 @@
+using RoomReservationApi.Models;
+
 namespace RoomReservationApi.Services;
 
-public class IReservationService
+public interface IReservationService
 {
-    
+    public List<Reservation> GetAll(DateOnly? dateFilter,ReservationStatus? statusFilter,long? roomIdFilter);
+    public Reservation Get(long reservationId);
+    public long AddReservation(long roomId,string organizerName,string topic,DateOnly date,TimeOnly startTime,TimeOnly endTime,ReservationStatus status);
+    public void UpdateReservation(long? roomId,string? organizerName,string? topic,DateOnly? date,TimeOnly? startTime,TimeOnly? endTime,ReservationStatus? status);
+    public void Delete(long id);
 }
