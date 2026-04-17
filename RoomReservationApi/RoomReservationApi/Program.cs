295eb2f7c78321bafd0277cbf9bc0cbdf131ed05
Franciszek Krawczyk <franciszek2005@gmail.com>
2026-04-17 12:13:01 +0200

    add: integration for the Room controllers, services and repos

--- RoomReservationApi/RoomReservationApi/Program.cs
+++ RoomReservationApi/RoomReservationApi/Program.cs
@@ -1,3 +1,7 @@
+using RoomReservationApi.Controllers;
+using RoomReservationApi.Repositories;
+using RoomReservationApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,14 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IRoomService,RoomService>();
+builder.Services.AddSingleton<IRoomRepository, RoomRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "RoomReservationApi v1");
+    });
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +29,4 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
